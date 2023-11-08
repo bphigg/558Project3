886e3a1f22fb00201427cc954ec7d8b90d8922d9
bphigg <bphigg@gmail.com>
2023-11-07 22:56:17 -0500

    summary stats bph

---
+++ summary stats.R
@@ -0,0 +1,37 @@
+#### Summary Stats
+library(ggplot2)
+library(corrplot)
+
+### Get a sense of what the diabetes rate is
+mean(diabetes$Diabetes_binary)
+
+### Group by Education
+diabetes %>% group_by(Education) %>% summarize(mean(Diabetes_binary), sd(Diabetes_binary))
+
+### Group by Sex
+diabetes %>% group_by(Sex) %>% summarize(mean(Diabetes_binary), sd(Diabetes_binary))
+
+### Contingency Tables
+table(diabetes$Diabetes_binary, diabetes$Education)
+table(diabetes$Diabetes_binary, diabetes$Income)
+
+table(diabetes$Education, diabetes$Income)
+
+### Histogram BMI/Diabetes
+
+g <- ggplot(diabetes, aes(x=BMI))
+g + geom_histogram(aes(fill = as.factor(diabetes$Diabetes_binary), y = ..density..), position = "dodge") +
+  labs(title = "Density Histogram - BMI", fill = "Diabetes")
+
+
+### Bar Plot Income/Diabetes
+g <- ggplot(diabetes, aes(x = Income))
+g + geom_bar(aes(fill = as.factor(Diabetes_binary)), position = "dodge") +
+  labs(x = "Education", title = "Bar Plot Income/Diabetes", y = "Count") +
+  scale_fill_discrete(name = "Diabetes", labels = c("No", "Yes"))
+
+### corrplot all numerical variables
+correlation <- cor(select(diabetes, Diabetes_binary, HighBP, HighChol, CholCheck, Stroke, HeartDiseaseorAttack, PhysActivity, Fruits, Veggies, HvyAlcoholConsump, AnyHealthcare, NoDocbcCost, DiffWalk, m_hlth, p_hlth), method = "spearman")
+corrplot(correlation, type = 'upper', tl.pos = 'lt')
+
+###
